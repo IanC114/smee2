e0dcbf95465f28021b9f2056e6440fd1cdd8dd18
alihanisarr <aliha.nra619@gmail.com>
2026-07-04 21:44:20 +0500

    added changes

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
-from logging import raiseExceptions
+from fastapi import raiseExceptions
 from fastapi import FastAPI, WebSocket, Request, HTTPException
 import uvicorn
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s.%(msecs)03dZ %(levelname)s:%(name)s:%(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=logging.INFO,
+)
+logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
+logging.getLogger("uvicorn.error").setLevel(logging.WARNING)
 
 app = FastAPI()
 
@@ -15,10 +24,10 @@ async def webhook(subscription_id: str, request: Request):
             raise HTTPException(status_code=403, detail="API key is not valid ")
 
         data = await request.json()
-        print("Webhook received: ", data)
+        logging.info("Webhook received: %s", data)
 
         subscribers[subscription_id] = data
-        client = clients[subscription_id]
+        client = clients.get(subscription_id)
 
         if client is not None:
             await client.send_json(data) 
